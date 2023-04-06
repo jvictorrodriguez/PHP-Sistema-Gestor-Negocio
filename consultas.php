4f0b0c25a756e978649fd40aaddb929f765d56d5
jvictorrodriguez <vrodriguezbarbeira@gmail.com>
2023-04-06 14:38:25 +0200

    Consultas acabado

--- consultas.php
+++ consultas.php
@@ -14,7 +14,6 @@
 		//Hacemos la consulta
 		$resultado = mysqli_query($conexion,$sql);
 		
-		
 		if (mysqli_num_rows($resultado) > 0) {
 			while ($fila = mysqli_fetch_assoc($resultado)) {
 				$enabled= $fila['enabled'];
@@ -22,13 +21,9 @@
 					return "autorizado";
 				return "registrado";
 			}
-		
 		}
-		return "no registrado";		
-		
+		return "no registrado";				
 	}
-	
-
 
 
 	function esSuperadmin($nombre, $correo){
@@ -57,47 +52,168 @@
 
 
 	function getPermisos() {
-		// Completar...	
+		/**Devuelve el valor del campo management */
+		//Conexión con la BBDD
+		$conexion = crearConexion("");
+		//Creamos la consulta
+		$sql = "SELECT * FROM setup";
+		//Hacemos la consulta y guardamos el resultado en $result
+		$result = mysqli_query($conexion, $sql);
+		if (mysqli_num_rows($result) > 0) {
+			while ($fila= mysqli_fetch_assoc($result)){ 
+				 return $fila["management"];
+			}
+		}
 	}
 
 
 	function cambiarPermisos() {
-		// Completar...	
+		/**Alterna/Cambia el valor del campo management */
+		// Primero revisamos los permisos
+		$permiso=getPermisos();
+		if ($permiso==0) $cambia=1;
+		else $cambia=0;
+
+		//Accedemos al registro de la tabla
+		$conexion = crearConexion("");
+		$sql = "UPDATE  setup SET management=$cambia";
+		$modifica = mysqli_query($conexion, $sql); 
+		cerrarConexion("");
+		
 	}
 
 
 	function getCategorias() {
-		// Completar...	
+		//Devuelve la lista de las categorías
+		$conexion = crearConexion("");
+		$sql = "SELECT id, name FROM category";
+		$result = mysqli_query($conexion, $sql); 
+		//Si la consulta ha devuelto algún valor, devolvemos los valores.
+		if (mysqli_num_rows($result) > 0) {
+			return $result;
+		}
+		// si no, enviamos un mensaje de error.
+		else {
+			return "No hay nada en la lista de categorías.";
+		}	
 	}
 
 
 	function getListaUsuarios() {
-		// Completar...	
+			// Devuelve una tabla virtual con los datos (full_name, email, enabled) 
+		//de todos los usuarios almacenados en la tabla user.
+		$conexion = crearConexion("");
+		$sql = "SELECT full_name, email, enabled FROM user";
+		
+		//Hacemos la consulta y guardamos el resultado en $result
+		$result = mysqli_query($conexion, $sql);
+		
+	  	//Si la consulta ha devuelto algún valor, devolvemos los valores.
+	    if (mysqli_num_rows($result) > 0) {
+			return $result;
+		}
+		// si no, enviamos un mensaje de error.
+		else {
+			return "No hay nada en la lista de usuarios.";
+		}
 	}
 
 
 	function getProducto($ID) {
-		// Completar...	
+		// Devuelve el registro correspondiente al id pasado por parámetro
+		$conexion = crearConexion("");
+		$sql = "SELECT id, name, cost, price, category_id FROM product WHERE id='$ID'";
+		
+		//Hacemos la consulta y guardamos el resultado en $result
+		$result = mysqli_query($conexion, $sql);
+		
+		//Si la consulta ha devuelto algún valor, devolvemos éste.
+		if (mysqli_num_rows($result) > 0) {
+			return $result;
+		}
+		// si no, enviamos un mensaje de error.
+		else {
+			return "No hay nada en la lista de productos.";
+		}
 	}
 
 
 	function getProductos($orden) {
-		// Completar...	
+		// Devuelve una tabla virtual con los datos de los productos 
+		$conexion = crearConexion("");
+		
+		$sql = "SELECT p.id, p.name, p.cost, p.price, p.category_id as categoria, c.name as category
+			 FROM product p INNER JOIN category c ON category_id = c.id order by $orden ";
+		
+
+		//Hacemos la consulta y guardamos el resultado en $result
+		$result = mysqli_query($conexion, $sql);
+		
+	  	//Si la consulta ha devuelto algún valor, devolvemos los valores.
+	    if (mysqli_num_rows($result) > 0) {
+			return $result;
+		}
+		// si no, enviamos un mensaje de error.
+		else {
+			return "No hay nada en la lista de usuarios.";
+		}
 	}
 
 
 	function anadirProducto($nombre, $coste, $precio, $categoria) {
-		// Completar...	
+		// Consulta SQL para añadir un producto
+		$conexion = crearConexion("");
+		$sql="INSERT INTO PRODUCT (name,cost,price,category_id) VALUES ('$nombre','$coste','$precio','$categoria')";
+		
+		
+		//Hacemos la consulta y guardamos el resultado en $result
+		$result = mysqli_query($conexion, $sql);
+		
+		//Si la consulta SQL de tipo INSERT ha sido correcta devuelve 1
+		if ($result==1) {
+			echo "Desde interior función anadirProducto ".$result;
+		}else{
+			echo "Desde interior función anadirProducto ".$result;
+		}
 	}
 
 
 	function borrarProducto($id) {
-		// Completar...	
+		// Consulta SQL para eliminar un producto
+		$conexion = crearConexion("");
+		$sql="DELETE FROM PRODUCT WHERE id='$id'";
+		//$sql="INSERT INTO PRODUCT (id,name,cost,price,category_id) VALUES ('$nombre','$coste','$precio','$categoria')";
+		
+		
+		//Hacemos la consulta y guardamos el resultado en $result
+		$result = mysqli_query($conexion, $sql);
+		
+		//Si la consulta SQL de tipo DELETE ha sido correcta devuelve 1
+		if ($result==1) {
+			echo "Desde interior función borrarProducto ".$result;
+		}else{
+			echo "Desde interior función borrarProducto ".$result;
+		}
+	
 	}
 
 
 	function editarProducto($id, $nombre, $coste, $precio, $categoria) {
-		// Completar...	
+			// Consulta SQL para editar un producto
+			$conexion = crearConexion("");
+			$sql="UPDATE product SET name='$nombre', cost='$coste', price='$precio', 
+			category_id='$categoria' WHERE id='$id'";
+			
+			
+			//Hacemos la consulta y guardamos el resultado en $result
+			$result = mysqli_query($conexion, $sql);
+			
+			//Si la consulta SQL de tipo DELETE ha sido correcta devuelve 1
+	if ($result==1) {
+		echo "Desde interior función editarProducto ".$result;
+	}else{
+		echo "Desde interior función editarProducto ".$result;
+	}
 	}
 
 ?>
