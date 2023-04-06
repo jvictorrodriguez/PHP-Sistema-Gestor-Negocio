72c44ae495583712b08cefef1fc7d988d466a981
jvictorrodriguez <vrodriguezbarbeira@gmail.com>
2023-04-06 09:45:18 +0200

    html ready

--- index.php
+++ index.php
@@ -11,7 +11,14 @@
 	
 		include "consultas.php";
 
+
 	?>
+	<h1>Inicio</h1>
+	<form action="index.php" method="post">
+		<label for="usuario">Usuario: </label><input type="text" name="usuario" id=""><br><br>
+		<label for="correo">Correo: </label> <input type="email" name="correo" id=""><br><br>
+		<input type="submit" value="Enviar">
+	</form>
 	
 	
 </body>
